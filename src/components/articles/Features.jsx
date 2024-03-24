c1b86c83b289889ae1772eb6e9128095ac4515ef
Maxime <maxime.rossbach@std.heh.be>
2024-03-24 10:36:42 +0100

    Page des articles finit mais non fonctionnelle

--- src/components/articles/Features.jsx
+++ src/components/articles/Features.jsx
@@ -1,7 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Features() {
     return (
         <div>
-            <h1>Features</h1>
+            <div className="mx-auto container">
+                <div className="flex justify-center md:px-5 xl:px-40 pt-10">
+                    <div className="bg-white w-72 inline-block border rounded-md shadow-lg pt-2 pb-2 md:w-full">
+                        <h1 className="text-center font-semibold mx-6 md:text-start">
+                            La nouvelle attraction de walibi arrive enfin !!!!
+                        </h1>
+                        <p className="mx-6 text-center md:text-start">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Ipsa vitae omnis voluptatum, eaque facere
+                            possimus.
+                        </p>
+                        <div className="grid grid-cols-2">
+                            <p className="pl-4 pt-4">Walibi</p>
+                            <div className="flex justify-end">
+                                <div className="grid grid-cols-2">
+                                    <img
+                                        className="pt-4 w-4"
+                                        src="assets/img/mess.png"
+                                        alt=""
+                                    />
+                                    <span className="pt-3 pl-1">2</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
